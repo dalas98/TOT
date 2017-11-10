8b8e1e2f1f6c1b93aa29c77e320b8048b78066ae
dalas98 <dalas98@gmail.com>
2017-11-10 11:45:46 +0700

    added php and css
    add update

---
+++ test.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Belajar HTML CSS</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<h1>Belajar HTML</h1>
+		<h1 id="mercubuana">Mercu Buana</h1>
+		<h1 class="mercu"></h1>
+		<a href="#" class="button">Link Button</a>
+		<table>
+		<thead>
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>John</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+      </tr>
+      <tr>
+        <td>Mary</td>
+        <td>Moe</td>
+        <td>mary@example.com</td>
+      </tr>
+      <tr>
+        <td>July</td>
+        <td>Dooley</td>
+        <td>july@example.com</td>
+      </tr>
+    </tbody>
+  </table>
+	</body>
+</html>
